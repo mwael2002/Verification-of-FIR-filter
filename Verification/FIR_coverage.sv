c890a65fdce6be85a04a7b656332f6c7274984c3
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2024-12-22 20:20:03 +0200

    Add files via upload

--- Verification/FIR_coverage.sv
+++ Verification/FIR_coverage.sv
@@ -9,13 +9,33 @@ import uvm_pkg::*;
 class FIR_coverage extends uvm_component;
     `uvm_component_utils(FIR_coverage)
 
+    parameter N=16;
+    logic reset; 
+    logic signed [N-1:0] noisy_signal;
+    // Array of output signal points
+    logic signed [N-1:0] filtered_signal;
+
     uvm_analysis_export #(FIR_seq_item) cov_export;
     uvm_tlm_analysis_fifo #(FIR_seq_item) cov_fifo ;
     FIR_seq_item cov_seq_item;
 
+    covergroup c1;
+    
+    c_reset: coverpoint this.reset;
+    c_noisy_signal: coverpoint this.noisy_signal{
+        option.auto_bin_max=512;
+    }
+    c_filtered_signal: coverpoint this.filtered_signal{
+        option.auto_bin_max=512;
+        ignore_bins ignore_range_pos []= {[signed'(16'b0010_0000_0000_0000):signed'(16'b0111_1111_1111_1111)]};
+        ignore_bins ignore_range_neg []= {[signed'(16'b1000_0000_0000_0000):signed'(16'b1110_0000_0000_0000)]};
+    }
+    
+    endgroup
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
+        this.c1=new();
     endfunction
 
     function void build_phase(uvm_phase phase);
@@ -32,10 +52,30 @@ class FIR_coverage extends uvm_component;
     function void connect_phase(uvm_phase phase);
         
         super.connect_phase(phase);
-        cov_fifo.connect(cov_export);
-        cov_fifo.size=32;
+        cov_export.connect(cov_fifo.analysis_export);
 
     endfunction
+
+    task run_phase(uvm_phase phase);
+            super.run_phase(phase);
+            
+
+	        //`uvm_info("run_phase","coverage run phase has started",UVM_LOW);
+            forever begin
+                int i,sig_length;
+	            cov_fifo.get(cov_seq_item);
+                sig_length=$size(cov_seq_item.noisy_signal);
+
+                for (i=0; i<sig_length;i++) begin
+                    this.reset=cov_seq_item.reset;
+                    this.noisy_signal=cov_seq_item.noisy_signal[i];
+                    this.filtered_signal=cov_seq_item.filtered_signal[i][2*N-1:N];
+                    this.c1.sample();
+
+                end
+                
+	        end
+    endtask 
 
 endclass
 
